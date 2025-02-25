ea93d51d9daf400a48395e398ecc73bbc157f17a
Xulusugol <maplessoms@outlook.com>
2025-02-25 23:51:35 +0800

    Deploy Hexo to gh-pages

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1740478643343";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1740498693175";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
